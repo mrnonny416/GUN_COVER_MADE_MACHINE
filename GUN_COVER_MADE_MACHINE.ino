3349d3de887aa0dbacf8e65d57ad8d9f0a738431
Mrnonny <68236769+mrnonny416@users.noreply.github.com>
2024-03-04 09:17:30 +0700

    Feat [mian ino] update new version of function base loop

--- GUN_COVER_MADE_MACHINE.ino
+++ GUN_COVER_MADE_MACHINE.ino
@@ -1,6 +1,6 @@
 #define SENSOR_TEMPERATURE_OUT 5  // write VCC
-#define BUTTON_PROCESS_OUT 7      // write VCC
-#define BUTTON_EMERGENCY_OUT 9    // write VCC
+#define BUTTON_PROCESS_OUT 40     // write VCC
+#define BUTTON_EMERGENCY_OUT 42   // write VCC
 
 #define SENSOR_SWITCH_FRONT_OUT 22   // write VCC
 #define SENSOR_SWITCH_BACK_OUT 23    // write VCC
@@ -23,7 +23,7 @@
 
 #define SENSOR_TEMPERATURE 4     // read
 #define BUTTON_PROCESS 6         // read
-#define BUTTON_EMERGENCY 8       // read
+#define BUTTON_EMERGENCY 7       // read
 #define SENSOR_SWITCH_FRONT 10   // read
 #define SENSOR_SWITCH_BACK 11    // read
 #define SENSOR_SWITCH_TOP 12     // read
@@ -40,19 +40,23 @@ bool IS_MOTOR_Y_WORK_POSITION = false;  // start Timer
 bool IS_COUNT_DOWN_HEAT = false;
 bool IS_COUNT_DOWN_PULL = false;
 unsigned long START_TIME = 0;
-int HEAT_MINUTE = 3;
-int PULL_MINUTE = 3;
+unsigned long HEAT_MINUTE = 1;
+unsigned long PULL_SECOND = 30;
 unsigned long TIMER_HEAT = HEAT_MINUTE * 60 * 1000;
-unsigned long TIMER_PULL = PULL_MINUTE * 60 * 1000;
-bool RETURN_X_MORTOR = false;
+unsigned long TIMER_PULL = PULL_SECOND * 1000;
+bool RETURN_X_MOTOR = false;
 bool RETURN_Y_MORTOR = false;
-bool PULL_WORK = false;
+bool IS_PULL_WORK = false;
+bool STOP_BUTTON_CLICK = false;
 bool RESET_WORK = false;
+bool IS_UP = true;
+bool IS_VACCUM_WORK = false;
+bool RESULT = false;
 // all process done set default
 
 void setup() {
   Serial.begin(115200);
-  Serial.println();
+  Serial.println("Setup");
   pinMode(SENSOR_SWITCH_FRONT_OUT, OUTPUT);
   pinMode(SENSOR_SWITCH_BACK_OUT, OUTPUT);
   pinMode(SENSOR_SWITCH_TOP_OUT, OUTPUT);
@@ -94,7 +98,7 @@ void setup() {
   digitalWrite(RELAY_2_LIG, HIGH);
 
   digitalWrite(VACCUM_RELAY, HIGH);  // active low
-  digitalWrite(LIGHT_YELLOW_RELAY, HIGH);
+  digitalWrite(LIGHT_YELLOW_RELAY, LOW);
   digitalWrite(LIGHT_BLUE_RELAY, HIGH);
   digitalWrite(LIGHT_GREEN_RELAY, HIGH);
 
@@ -107,187 +111,270 @@ void setup() {
 
   digitalWrite(SENSOR_TEMPERATURE_OUT, HIGH);
   digitalWrite(BUTTON_PROCESS_OUT, HIGH);
-  digitalWrite(BUTTON_EMERGENCY_OUT, HIGH);
+  digitalWrite(BUTTON_EMERGENCY_OUT, 1);
+  Serial.println("Heating And Waiting Button Action...");
 }
 
 void loop() {
-  if (IS_PROCESS_START) {
-    digitalWrite(LIGHT_YELLOW_RELAY, LOW);
-  }
+  buttonGreen();
+  // buttonRed();
+  subHeater();
+  heatingFlow();
+  heatCountdown();
+  pullFlow();
+  vaccumFlow();
+  pullCountdown();
+  genPulse();
+  disableMotorX();
+  disableMotorY();
+}
 
-  if (digitalRead(BUTTON_PROCESS) == HIGH && IS_PROCESS_START == true && IS_TEMP_LIMIT == false) {
-    IS_BUTTON_PROCESS_CLICK = true;
+void buttonRed() {
+  int value = digitalRead(BUTTON_PROCESS);
+  if (value == 1) {
+    Serial.println("Emergency click");
+    digitalWrite(SENSOR_SWITCH_FRONT_OUT, HIGH);
+    digitalWrite(SENSOR_SWITCH_BACK_OUT, HIGH);
+    digitalWrite(SENSOR_SWITCH_TOP_OUT, HIGH);
+    digitalWrite(SENSOR_SWITCH_BOTTON_OUT, HIGH);
+
+    digitalWrite(RELAY_1_VAC, HIGH);
+    digitalWrite(RELAY_2_LIG, HIGH);
+
+    digitalWrite(VACCUM_RELAY, HIGH);  // active low
+    digitalWrite(LIGHT_YELLOW_RELAY, HIGH);
+    digitalWrite(LIGHT_BLUE_RELAY, HIGH);
+    digitalWrite(LIGHT_GREEN_RELAY, HIGH);
+
+    digitalWrite(STEP_MOTOR_PULSE_X, HIGH);
+    digitalWrite(STEP_MOTOR_ENABLE_X, HIGH);
+    digitalWrite(STEP_MOTOR_DIRECT_X, HIGH);
+    digitalWrite(STEP_MOTOR_PULSE_Y, HIGH);
+    digitalWrite(STEP_MOTOR_ENABLE_Y, HIGH);
+    digitalWrite(STEP_MOTOR_DIRECT_Y, HIGH);  // LOW-UP
+
+    digitalWrite(SENSOR_TEMPERATURE_OUT, HIGH);
+    digitalWrite(BUTTON_PROCESS_OUT, HIGH);
+    digitalWrite(BUTTON_EMERGENCY_OUT, 1);
+    resetAll();
+  }
+}
+
+void buttonGreen() {
+  int value = digitalRead(BUTTON_PROCESS);
+  if (value == 1 && IS_PROCESS_START == true) {
+    Serial.println("Process click");
+    value = 0;
     IS_PROCESS_START = false;
-  }
-
-  if (IS_BUTTON_PROCESS_CLICK) {
+    IS_BUTTON_PROCESS_CLICK = true;
+    digitalWrite(VACCUM_RELAY, LOW);  // active low
     digitalWrite(LIGHT_YELLOW_RELAY, HIGH);
     digitalWrite(LIGHT_BLUE_RELAY, LOW);
+    digitalWrite(LIGHT_GREEN_RELAY, LOW);
+    moveMotorX("out");
+    moveMotorY("up");
+    Serial.println("wait temp");
+  }
+}
 
+void subHeater() {
+  if (IS_BUTTON_PROCESS_CLICK && digitalRead(STEP_MOTOR_ENABLE_X) == HIGH && digitalRead(STEP_MOTOR_ENABLE_Y) == HIGH) {
+    int value = digitalRead(SENSOR_TEMPERATURE);
+    Serial.println("waiting...");
+    Serial.println(value);
+    if (value == 1) {
+      Serial.println("Temp is Limit");
+      digitalWrite(VACCUM_RELAY, LOW);  // active low
+      digitalWrite(LIGHT_YELLOW_RELAY, LOW);
+      digitalWrite(LIGHT_BLUE_RELAY, HIGH);
+      digitalWrite(LIGHT_GREEN_RELAY, LOW);
+      IS_BUTTON_PROCESS_CLICK = false;
+      IS_TEMP_LIMIT = true;
+    }
+  }
+}
+
+void heatingFlow() {
+  if (IS_TEMP_LIMIT) {
+    digitalWrite(VACCUM_RELAY, LOW);  // active low
+    digitalWrite(LIGHT_YELLOW_RELAY, LOW);
+    digitalWrite(LIGHT_BLUE_RELAY, LOW);
+    digitalWrite(LIGHT_GREEN_RELAY, HIGH);
+    Serial.println("Start Heating");
+    moveMotorX("in");
+    IS_TEMP_LIMIT = false;
+    IS_COUNT_DOWN_HEAT = true;
+    START_TIME = millis();
+  }
+}
+
+void heatCountdown() {
+  if (IS_COUNT_DOWN_HEAT && digitalRead(STEP_MOTOR_ENABLE_X) == HIGH) {
+    Serial.println("start Counting");
+    Serial.println((TIMER_HEAT - (millis() - START_TIME)) / 1000);
+    Serial.println("=========================================");
+    delay(1000);
+    if (millis() - START_TIME >= TIMER_HEAT) {
+      Serial.println("motor x out");
+      IS_COUNT_DOWN_HEAT = false;
+      moveMotorX("out");
+      IS_PULL_WORK = true;
+    }
+  }
+}
+
+void pullFlow() {
+  if (IS_PULL_WORK && digitalRead(STEP_MOTOR_ENABLE_X) == HIGH) {
+    Serial.println("motor y work");
+    IS_PULL_WORK = false;
+    moveMotorY("down");
+    IS_VACCUM_WORK = true;
+  }
+}
+
+void vaccumFlow() {
+  if (IS_VACCUM_WORK && digitalRead(STEP_MOTOR_ENABLE_Y) == HIGH) {
+    Serial.println("vaccum work");
+    IS_COUNT_DOWN_PULL = true;
+    IS_VACCUM_WORK = false;
+    moveVaccum("on");
+    START_TIME = millis();
+  }
+}
+
+void pullCountdown() {
+  if (IS_COUNT_DOWN_PULL) {
+    Serial.println("start Counting");
+    Serial.println((TIMER_PULL - (millis() - START_TIME)) / 1000);
+    Serial.println("=========================================");
+    delay(1000);
+    if (millis() - START_TIME >= TIMER_PULL) {
+      Serial.println("work done");
+      IS_COUNT_DOWN_PULL = false;
+      moveVaccum("off");
+      moveMotorY("up");
+      RESULT = true;
+    }
+  }
+}
+
+void setStart() {
+  if (RESULT && digitalRead(STEP_MOTOR_ENABLE_Y) == LOW) {
+    resetAll();
+  }
+}
+
+void resetAll() {
+  Serial.println("Please Click Green Button to continute");
+  IS_PROCESS_START = true;  // yellow start
+  IS_MOTOR_X_START_POSITION = false;
+  IS_MOTOR_Y_START_POSITION = false;
+  IS_BUTTON_PROCESS_CLICK = false;   // blue start
+  IS_TEMP_LIMIT = false;             // green start
+  IS_PROCESSION = false;             // start Timer
+  IS_MOTOR_X_WORK_POSITION = false;  // start Timer
+  IS_MOTOR_Y_WORK_POSITION = false;  // start Timer
+  IS_COUNT_DOWN_HEAT = false;
+  IS_COUNT_DOWN_PULL = false;
+  RETURN_X_MOTOR = false;
+  RETURN_Y_MORTOR = false;
+  IS_PULL_WORK = false;
+  RESET_WORK = false;
+  STOP_BUTTON_CLICK = false;
+  IS_VACCUM_WORK = false;
+  RESULT = false;
+}
+
+void moveMotorX(String direct) {
+  Serial.println("Motor X Controller");
+  if (direct == "out") {
+    digitalWrite(STEP_MOTOR_DIRECT_X, LOW);
+    digitalWrite(STEP_MOTOR_ENABLE_X, LOW);
+  }
+  if (direct == "in") {
     digitalWrite(STEP_MOTOR_DIRECT_X, HIGH);
-    digitalWrite(STEP_MOTOR_DIRECT_Y, LOW);  // up
+    digitalWrite(STEP_MOTOR_ENABLE_X, LOW);
+  }
+}
 
+void moveMotorY(String direct) {
+  Serial.println("Motor Y Controller");
+  if (direct == "up") {
+    digitalWrite(STEP_MOTOR_DIRECT_Y, LOW);
+    digitalWrite(STEP_MOTOR_ENABLE_Y, LOW);
+  }
+  if (direct == "down") {
+    digitalWrite(STEP_MOTOR_DIRECT_Y, HIGH);
+    digitalWrite(STEP_MOTOR_ENABLE_Y, LOW);
+  }
+}
+
+void genPulse() {
+  int x_value = digitalRead(STEP_MOTOR_ENABLE_X);
+  int y_value = digitalRead(STEP_MOTOR_ENABLE_Y);
+  if (x_value == 0 || y_value == 0) {
     digitalWrite(STEP_MOTOR_PULSE_X, HIGH);
     digitalWrite(STEP_MOTOR_PULSE_Y, HIGH);
     delay(1);
     digitalWrite(STEP_MOTOR_PULSE_X, LOW);
+    digitalWrite(STEP_MOTOR_PULSE_Y, LOW);
+  }
+}
 
-    if (digitalRead(SENSOR_SWITCH_FRONT) == LOW) {
-      IS_MOTOR_X_START_POSITION = true;
-    }
-    if (digitalRead(SENSOR_SWITCH_TOP) == LOW) {
-      IS_MOTOR_Y_START_POSITION = true;
-    }
-
-    if (!IS_MOTOR_X_START_POSITION) {
-      digitalWrite(STEP_MOTOR_ENABLE_X, LOW);
+void disableMotorX() {
+  if (digitalRead(STEP_MOTOR_ENABLE_X) == LOW) {
+    if (digitalRead(STEP_MOTOR_DIRECT_X) == LOW) {
+      if (digitalRead(SENSOR_SWITCH_FRONT) != digitalRead(SENSOR_SWITCH_FRONT_OUT)) {
+        Serial.println("motor x stop");
+        digitalWrite(STEP_MOTOR_ENABLE_X, HIGH);
+      } else {
+        int value = digitalRead(SENSOR_SWITCH_FRONT_OUT);
+        digitalWrite(SENSOR_SWITCH_FRONT_OUT, !value);
+      }
     } else {
-      digitalWrite(STEP_MOTOR_ENABLE_X, HIGH);
-    }
-
-    if (!IS_MOTOR_Y_START_POSITION) {
-      digitalWrite(STEP_MOTOR_ENABLE_Y, LOW);
-    } else {
-      digitalWrite(STEP_MOTOR_ENABLE_Y, HIGH);
-    }
-
-    if (digitalRead(SENSOR_TEMPERATURE) == HIGH && IS_MOTOR_X_START_POSITION && IS_MOTOR_Y_START_POSITION) {
-      IS_TEMP_LIMIT = true;
-      IS_BUTTON_PROCESS_CLICK = false;
+      if (digitalRead(SENSOR_SWITCH_BACK) != digitalRead(SENSOR_SWITCH_BACK_OUT)) {
+        Serial.println("motor x stop");
+        digitalWrite(STEP_MOTOR_ENABLE_X, HIGH);
+      } else {
+        int value = digitalRead(SENSOR_SWITCH_BACK_OUT);
+        digitalWrite(SENSOR_SWITCH_BACK_OUT, !value);
+      }
     }
   }
+}
 
-  if (IS_TEMP_LIMIT) {
+void disableMotorY() {
+  if (digitalRead(STEP_MOTOR_ENABLE_Y) == LOW) {
+    if (digitalRead(STEP_MOTOR_DIRECT_Y) == LOW) {
+      if (digitalRead(SENSOR_SWITCH_TOP) != digitalRead(SENSOR_SWITCH_TOP_OUT)) {
+        Serial.println("motor y stop");
+        digitalWrite(STEP_MOTOR_ENABLE_Y, HIGH);
+      } else {
+        int value = digitalRead(SENSOR_SWITCH_TOP_OUT);
+        digitalWrite(SENSOR_SWITCH_TOP_OUT, !value);
+      }
+    } else {
+      if (digitalRead(SENSOR_SWITCH_BOTTON) != digitalRead(SENSOR_SWITCH_BOTTON_OUT)) {
+        Serial.println("motor y stop");
+        digitalWrite(STEP_MOTOR_ENABLE_Y, HIGH);
+      } else {
+        int value = digitalRead(SENSOR_SWITCH_BOTTON_OUT);
+        digitalWrite(SENSOR_SWITCH_BOTTON_OUT, !value);
+      }
+    }
+  }
+}
+
+void moveVaccum(String swt) {
+  if (swt == "on") {
+    digitalWrite(VACCUM_RELAY, HIGH);  // active low
+    digitalWrite(LIGHT_YELLOW_RELAY, LOW);
+    digitalWrite(LIGHT_BLUE_RELAY, LOW);
+    digitalWrite(LIGHT_GREEN_RELAY, HIGH);
+  } else {
+    digitalWrite(VACCUM_RELAY, LOW);  // active low
+    digitalWrite(LIGHT_YELLOW_RELAY, HIGH);
     digitalWrite(LIGHT_BLUE_RELAY, LOW);
     digitalWrite(LIGHT_GREEN_RELAY, LOW);
-
-    digitalWrite(STEP_MOTOR_DIRECT_X, HIGH);
-    digitalWrite(STEP_MOTOR_DIRECT_Y, HIGH);  // up
-
-    digitalWrite(STEP_MOTOR_PULSE_X, HIGH);
-    digitalWrite(STEP_MOTOR_PULSE_Y, HIGH);
-    delay(1);
-    digitalWrite(STEP_MOTOR_PULSE_X, LOW);
-    digitalWrite(STEP_MOTOR_PULSE_Y, LOW);
-    // x sensor
-    IS_MOTOR_X_START_POSITION = false;
-    if (digitalRead(SENSOR_SWITCH_BACK) == LOW) {
-      IS_MOTOR_X_WORK_POSITION = true;
-    }
-    // run motor
-    if (!IS_MOTOR_X_WORK_POSITION) {
-      digitalWrite(STEP_MOTOR_ENABLE_X, LOW);
-    } else {
-      digitalWrite(STEP_MOTOR_ENABLE_X, HIGH);
-      IS_TEMP_LIMIT = false;
-      IS_COUNT_DOWN_HEAT = true;
-      START_TIME = millis();
-    }
-  }
-
-  if (IS_COUNT_DOWN_HEAT) {
-    if (millis() - START_TIME >= TIMER_HEAT) {
-      IS_COUNT_DOWN_HEAT = false;
-      RETURN_X_MORTOR = true;
-    }
-  }
-
-  if (RETURN_X_MORTOR) {
-    digitalWrite(STEP_MOTOR_DIRECT_X, HIGH);
-    digitalWrite(STEP_MOTOR_DIRECT_Y, LOW);  // up
-
-    digitalWrite(STEP_MOTOR_PULSE_X, HIGH);
-    digitalWrite(STEP_MOTOR_PULSE_Y, HIGH);
-    delay(1);
-    digitalWrite(STEP_MOTOR_PULSE_X, LOW);
-    digitalWrite(STEP_MOTOR_PULSE_Y, LOW);
-    // x sensor
-    IS_MOTOR_X_START_POSITION = false;
-    if (digitalRead(SENSOR_SWITCH_FRONT) == LOW) {
-      IS_MOTOR_X_START_POSITION = true;
-    }
-    // run motor
-    if (!IS_MOTOR_X_START_POSITION) {
-      digitalWrite(STEP_MOTOR_ENABLE_X, LOW);
-    } else {
-      digitalWrite(STEP_MOTOR_ENABLE_X, HIGH);
-      RETURN_X_MORTOR = false;
-      PULL_WORK = true;
-    }
-  }
-
-  if (PULL_WORK) {
-    digitalWrite(VACCUM_RELAY, LOW);
-    digitalWrite(STEP_MOTOR_DIRECT_X, HIGH);
-    digitalWrite(STEP_MOTOR_DIRECT_Y, HIGH);  // down
-
-    digitalWrite(STEP_MOTOR_PULSE_X, HIGH);
-    digitalWrite(STEP_MOTOR_PULSE_Y, HIGH);
-    delay(1);
-    digitalWrite(STEP_MOTOR_PULSE_X, LOW);
-    digitalWrite(STEP_MOTOR_PULSE_Y, LOW);
-
-    IS_MOTOR_Y_WORK_POSITION = false;
-    if (digitalRead(SENSOR_SWITCH_BOTTON) == LOW) {
-      IS_MOTOR_X_WORK_POSITION = true;
-    }
-    // run motor
-    if (!IS_MOTOR_X_WORK_POSITION) {
-      digitalWrite(STEP_MOTOR_ENABLE_X, LOW);
-    } else {
-      digitalWrite(STEP_MOTOR_ENABLE_X, HIGH);
-      PULL_WORK = false;
-      IS_COUNT_DOWN_PULL = true;
-      START_TIME = millis();
-    }
-  }
-
-  if (IS_COUNT_DOWN_PULL) {
-    if (millis() - START_TIME >= TIMER_PULL) {
-      IS_COUNT_DOWN_PULL = false;
-      RETURN_Y_MORTOR = true;
-    }
-  }
-
-  if (RETURN_Y_MORTOR) {
-    digitalWrite(STEP_MOTOR_DIRECT_X, HIGH);
-    digitalWrite(STEP_MOTOR_DIRECT_Y, LOW);  // up
-
-    digitalWrite(STEP_MOTOR_PULSE_X, HIGH);
-    digitalWrite(STEP_MOTOR_PULSE_Y, HIGH);
-    delay(1);
-    digitalWrite(STEP_MOTOR_PULSE_X, LOW);
-    digitalWrite(STEP_MOTOR_PULSE_Y, LOW);
-    // x sensor
-    IS_MOTOR_Y_START_POSITION = false;
-    if (digitalRead(SENSOR_SWITCH_FRONT) == LOW) {
-      IS_MOTOR_Y_START_POSITION = true;
-    }
-    // run motor
-    if (!IS_MOTOR_Y_START_POSITION) {
-      digitalWrite(STEP_MOTOR_ENABLE_Y, LOW);
-    } else {
-      digitalWrite(STEP_MOTOR_ENABLE_Y, HIGH);
-      RETURN_Y_MORTOR = false;
-      digitalWrite(VACCUM_RELAY, HIGH);
-      RESET_WORK = true;
-    }
-  }
-
-  if (RESET_WORK) {
-    IS_PROCESS_START = true;  // yellow start
-    IS_MOTOR_X_START_POSITION = false;
-    IS_MOTOR_Y_START_POSITION = false;
-    IS_BUTTON_PROCESS_CLICK = false;   // blue start
-    IS_TEMP_LIMIT = false;             // green start
-    IS_PROCESSION = false;             // start Timer
-    IS_MOTOR_X_WORK_POSITION = false;  // start Timer
-    IS_MOTOR_Y_WORK_POSITION = false;  // start Timer
-    IS_COUNT_DOWN_HEAT = false;
-    IS_COUNT_DOWN_PULL = false;
-    RETURN_X_MORTOR = false;
-    RETURN_Y_MORTOR = false;
-    PULL_WORK = false;
-    RESET_WORK = false;
   }
 }
